cbfb21fa7ccb35f9ad183b11c8d1eec856ed9ab6
Jake Chavez <jake94chavez@gmail.com>
2017-12-12 16:34:54 -0600

    Finished afternoon lab. Morning lab WIP

--- answers.js
+++ answers.js
@@ -1,6 +1,6 @@
 // Morning lab
 // Given, the following object, log the third element from the array
-const fun = {
+/*const fun = {
     asdf: ["afd", "matt", 'sweet']
 }
 console.log(fun.asdf[2]);
@@ -64,14 +64,134 @@ const container = [
     ['bear', 'dog', 'cat'],
     [true, false, true]
 ];
+
+// Challenge from Shahzad
 for (let value of container) {
 	for (i = 0; i < container.length; i++) {
 	console.log(value[i]);
 	}
 }
+*/
 
 
+/////////////////
+//Afternoon lab//
+/////////////////
 
+// 1. Log the following function"
+//const foo = () => {
+//	console.log('I have been invoked!');
+//}
+
+//foo();
+
+// 2. Call `foo`, passing in a function
+// const foo = (param) => {
+// 	console.log(param);
+// }
+
+// console.log('banana');
+
+// 3. Guess what this will log:
+// const foo = (param, param2) => {
+// 	param(param2);
+// }
+
+// const bar = (param) => {
+// 	console.log(param);
+// }
+
+// log an error?
+// foo(bar, 'hi');
+// Nope. It just logged hi
+
+// 4. Guess what this will log:
+// const foo = (param, param2) => {
+//     param(param2, 'hello');
+// }
+
+// const bar = (param, param2) => {
+//     console.log(param2);
+// }
+
+// foo(bar, 'hi');
+
+// hello
+// I was right this time!
+
+// 5. Call the someMethod function, passing in myFunc. It should log About to invoke the callback and then I have been called!
+// const foo = {
+//     someMethod(callback){
+//         console.log('About to invoke the callback');
+//         callback();
+//     }
+// }
+
+// const myFunc = () =>{
+//     console.log('I have been called!');
+// }
+
+//foo.someMethod(myFunc);
+
+// 6. Alter the following code so that myFunc properly logs the height property of foo
+// Not exactly sure what this question is asking. Does it want myFunc to get height when called through someMethod? Or just on its own?
+// const foo = {
+//     height:5,
+//     someMethod(callback){
+//         callback();
+//     }
+// }
+
+// // const myFunc = (param)=>{
+// //     console.log(param);
+// // }
+
+// const myFunc = (param) => {
+// 	console.log(param.height);
+// }
+
+// myFunc(foo);
+
+// 7. Create a method called combineColor on the shirtColor object that combines all the elements of the color array into a string
+const shirtColor = {
+	color: ['red', 'blue', 'green', 'white', 'pink'],
+	shirtMethod() {
+		console.log(shirtColor.color.join());
+	}
+}
+
+shirtColor.shirtMethod();
+
+let wordReverse = (input) => {
+	return input.split(' ').reverse().join(' ');
+}
+
+let toUpperCase = (input) => {
+	return input.toUpperCase();
+}
+
+let repMaster = (input, func) => {
+	console.log(func(input) + " proves that I am the rep MASTER!");
+}
+
+repMaster("Never give your heart to a blockead", wordReverse);
+repMaster("I finished this practice", toUpperCase);
+
+// 8. Fix the last line of this snippet so it logs this method was called:
+const bar = {
+    someMethod(param){
+        param();
+    }
+}
+
+const foo = {
+    someMethod(){
+        console.log('this method was called');
+    }
+}
+
+//bar.someMethod; //fix this line
+bar.someMethod(foo.someMethod);
 
 
 
